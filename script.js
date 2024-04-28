cf6e14982d496500de5b9a69916ff92eb1f7c684
lalitrajawat <153417423+lalitrajawat@users.noreply.github.com>
2024-04-28 14:38:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+
+function display(value) {
+    let string = document.getElementById("res");
+    while(string.value=="0"){
+        string.value = "";
+        break;
+    }
+   // let string2 = document.getElementById("screen");
+    string.value += value;
+}
+
+// Function to clear the screen
+function clearScreen() {
+    let string = document.getElementById("res");
+    string.value = "0";
+}
+
+function clearScreen2() {
+    let string = document.getElementById("res");
+    string.value = "0";
+    let string2 = document.getElementById("screen");
+    string2.value = "";
+}
+
+function back(){
+    let string = document.getElementById("res");
+    string.value = string.value.slice(0, -1);
+}
+
+
+function calculate() {
+    let string = document.getElementById("res");
+    let string2 = document.getElementById("screen");
+    string2.value = string.value;
+
+    let result = eval(string.value);
+    
+    string.value = result;
+
+}
