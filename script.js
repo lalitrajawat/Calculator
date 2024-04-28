8185fc62a51a467c8b060dd87346678028b894cf
lalitrajawat <153417423+lalitrajawat@users.noreply.github.com>
2024-04-28 22:42:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ function display(value) {
         break;
     }
    // let string2 = document.getElementById("screen");
-    window.navigator.vibrate(500);
     string.value += value;
 }
 
@@ -39,3 +38,14 @@ function calculate() {
     string.value = result;
 
 }
+const button = document.getElementById('vibrate-button');
+
+button.addEventListener('click', () => {
+  // Check if the Vibration API is supported
+  if ('vibrate' in navigator) {
+    // Vibrate for 200 milliseconds
+    navigator.vibrate(200);
+  } else {
+    console.log('Vibration API is not supported');
+  }
+});
